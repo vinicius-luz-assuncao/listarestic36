202913728b8f09f559eabe79f88675a28e1bc4e4
Vinícius Luz Assunção <vinicius.luz.foto@gmail.com>
2024-10-05 19:43:03 -0300

    comp: cards

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -1,12 +1,18 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { ProductsService } from '../../shared/services/products.service';
 import { Product } from '../../shared/interfaces/product.interface';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss',
 })
@@ -17,7 +23,7 @@ export class ListComponent {
 
   ngOnInit() {
     this.productsService.getAll().subscribe((products) => {
-      this.products =products;
-    })
+      this.products = products;
+    });
   }
 }
