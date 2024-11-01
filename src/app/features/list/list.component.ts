b71319f1ec0a796a9d1fd16765eacb50f2c38880
vinicius-luz-assuncao <vinicius.luz.foto@gmail.com>
2024-11-01 16:53:26 -0300

    Edit: Cria Dialog

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -4,9 +4,26 @@ import { Product } from '../../shared/interfaces/product.interface';
 import { CardComponent } from './components/card/card.component';
 import { Router, RouterLink } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialog, MatDialogModule} from '@angular/material/dialog';
 import { MatIcon } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-confirmation-dialog',
+  template: `<h2 mat-dialog-title>Delete file</h2>
+  <mat-dialog-content>
+    Would you like to delete cat.jpeg?
+  </mat-dialog-content>
+  <mat-dialog-actions>
+    <button mat-button mat-dialog-close>No</button>
+    <button mat-button mat-dialog-close cdkFocusInitial>Ok</button>
+  </mat-dialog-actions>`,
+  standalone: true,
+  imports: [MatButtonModule, MatDialogModule],
+})
+export class ConfirmationDialogComponent {}
+
 
 
 @Component({
@@ -22,6 +39,7 @@ export class ListComponent {
   productsService = inject(ProductsService);
   router = inject(Router);
 trackByProductId: TrackByFunction<Product> | undefined;
+matDialog = inject(MatDialog);
   
 
   ngOnInit() {
@@ -34,6 +52,10 @@ trackByProductId: TrackByFunction<Product> | undefined;
   }
 
   onDelete(product: Product) {
+    this.matDialog.open(ConfirmationDialogComponent)
+    .afterClosed()
+    .subscribe((data) => {console.log('afterclosed', data);})  
+    
     this.productsService.delete(product.id).subscribe(() => {
       this.products = this.products.filter(p => p.id !== product.id);
     });  
