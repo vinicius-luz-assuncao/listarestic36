5296cb67d2b8a00c83951cef5dee6e3a52263a8e
Vinícius Luz Assunção <vinicius.luz.foto@gmail.com>
2024-11-05 02:37:02 -0300

    Fix: começa editar estilo e tirar exemplos planejados

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -108,24 +108,5 @@ export class ListComponent {
       });
   }
 
-  todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
-
-  done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
-
-  drop(event: CdkDragDrop<string[]>) {
-    if (event.previousContainer === event.container) {
-      moveItemInArray(
-        event.container.data,
-        event.previousIndex,
-        event.currentIndex
-      );
-    } else {
-      transferArrayItem(
-        event.previousContainer.data,
-        event.container.data,
-        event.previousIndex,
-        event.currentIndex
-      );
-    }
-  }
+ 
 }
